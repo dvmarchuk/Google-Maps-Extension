43910ef2234bbd40a5970021bedf724eeb96f90f
Dennis Marchuk <dvmarchuk4@gmail.com>
2021-06-21 23:42:13 +0300

    very small update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ chrome.contextMenus.onClicked.addListener(function(info) {
 
 searchGoogleMaps = function(info){
     var query = info.selectionText;
-    chrome.tabs.create({url: "https://www.google.com/maps/dir/My+Location/" + query}); //for production. 
+    chrome.tabs.create({url: "https://www.google.com/maps/dir/My+Location/" + query}); //to search from your current location
  };
 
  
